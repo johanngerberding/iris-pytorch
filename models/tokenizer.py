b8b322c2ab860d2d6c063614e5b9dbe06bd5e398
Johann Gerberding <johann.gerberding@lector.ai>
2024-02-10 15:14:34 +0100

    attention block

--- models/tokenizer.py
+++ models/tokenizer.py
@@ -26,6 +26,8 @@ class TokenizerConfig:
     embed_dim: int 
     encoder_cfg: EncoderConfig
     decoder_cfg: DecoderConfig
+    dropout: float 
+    attn_resolutions: List[int]
 
 
 class LossWithIntermediateLosses:
@@ -46,6 +48,64 @@ def Normalize(in_channels: int) -> nn.Module:
 def swish(x: torch.Tensor) -> torch.Tensor: 
     return x * torch.sigmoid(x)
 
+
+class AttnBlock(nn.Module):
+    def __init__(self, in_channels: int) -> None: 
+        super().__init__()
+        self.in_channels = in_channels 
+        self.norm = Normalize(in_channels)
+        self.q = nn.Conv2d(
+            in_channels=in_channels, 
+            out_channels=in_channels, 
+            kernel_size=1, 
+            stride=1,
+            padding=0,
+        )
+        self.k = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=in_channels,
+            kernel_size=1, 
+            stride=1,
+            padding=0,
+        )
+        self.v = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=in_channels,
+            kernel_size=1, 
+            stride=1,
+            padding=0,
+        )
+        self.proj_out = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=in_channels,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+        )
+
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        h_ = x 
+        h_ = self.norm(h_) 
+        q = self.q(h_)
+        k = self.k(h_)
+        v = self.v(h_)
+
+        b, c, h, w = q.shape 
+        q = q.reshape(b, c, h * w)
+        q = q.permute(0, 2, 1)
+        k = k.reshape(b, c, h * w)
+        w_ = torch.bmm(q, k)
+        w_ = w_ * (int(c) ** (-0.5))
+        w_ = nn.functional.softmax(w_, dim=2)
+        
+        v = v.reshape(b, c, h * w) 
+        w_ = w_.permute(0, 2, 1)
+        h_ = torch.bmm(v, w_)
+        h_ = h_.reshape(b, c, h, w)
+        h_ = self.proj_out(h_)
+
+        return x + h_
 
 class ResnetBlock(nn.Module): 
     def __init__(
@@ -137,9 +197,16 @@ class Encoder(nn.Module):
 
             for b in range(self.config.num_res_blocks):
                 block.append(
-
+                    ResnetBlock(
+                        in_channels=block_in, 
+                        out_channels=block_out, 
+                        temb_channels=self.timestep_embedding_channels,
+                        dropout=cfg.dropout
+                    )
                 )
-
+                block_in = block_out
+                if curr_resolution in cfg.attn_resolutions:
+                    attn.append(AttnBlock(block_in))
 
 
 
